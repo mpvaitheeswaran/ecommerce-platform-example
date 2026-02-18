2110379c5bc901cf3d7d9f7d11756fdf79835d00
mpvaitheeswaran <mpvaitheeswaran@gmail.com>
2026-02-18 19:22:01 +0530

    Initial commit: Dockerized multi-module Spring Boot ecommerce platform

---
+++ services/service-registry/src/test/java/com/mpvaitheeswaran/ecommerce/service_registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpvaitheeswaran.ecommerce.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
