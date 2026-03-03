70159faf2b2bb2ee4a5d81e2e9af8489451cda5b
mpvaitheeswaran <mpvaitheeswaran@gmail.com>
2026-03-03 21:13:39 +0530

    auth controller created

---
+++ services/auth-service/src/main/java/com/mpvaitheeswaran/ecommerce/auth_service/controller/AuthController.java
@@ -0,0 +1,15 @@
+package com.mpvaitheeswaran.ecommerce.auth_service.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+
+    @PostMapping("/register")
+    public String register() {
+        return "User registered successfully!";
+    }
+}
